b9cc72b23657ed37d7344adb7314151f0682e643
unknown <arixxdelera21@gmail.com>
2023-05-21 10:55:49 +0800

    pseudocode has been added

--- OOP_Calculator.py
+++ OOP_Calculator.py
@@ -1,1 +1,7 @@
 # Delera, Aritz B.
+
+# Pseudocode
+# Create a seperate file for class input
+# Create a separate file for class Operations
+# Create a separate file for class display
+# Create a separate file for class calculator
