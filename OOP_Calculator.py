34389d705669c46163c81833be79ca87209f2366
unknown <arixxdelera21@gmail.com>
2023-05-21 11:46:04 +0800

    calculator_input is working yey

--- OOP_Calculator.py
+++ OOP_Calculator.py
@@ -1,7 +1,23 @@
 # Delera, Aritz B.
 
 # Import the calculator_input
+from calculator_input import CalculatorInput
+
 # cretae a class calculator
-# use a Non-Parametrized Constructor
-# initialize the calculator_input
-# test if numbers are able to output
+class Calculator:
+    # use a Non-Parametrized Constructor
+    def __init__(self):
+        # initialize the calculator_input
+        self.input = CalculatorInput()
+         
+    # test if numbers are able to output
+    def test(self):
+     number_1 = self.input.get_user_number("first")
+     number_2 = self.input.get_user_number("second")
+
+     result = number_1 + number_2
+     return result
+
+calculator = Calculator()
+result = calculator.test()
+print("Result:", result)
--- calculator_input.py
+++ calculator_input.py
@@ -1,4 +1,10 @@
 # define a class input
-# create a method to get the user's operatiom
-# create a method to get the user's number
+class CalculatorInput:
+    # create a method to get the user's operatiom
+    def get_operation(self):
+        return input("\033[31mWhich of the available operators would you like to make use of?: \033[0m")
+
+    # create a method to get the user's number
+    def get_user_number(self, order):
+        return float(input("\033[32mType in the {order} number of what you wish to calculate: \033[0m"))
  
