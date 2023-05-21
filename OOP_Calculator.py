5222b9ed6fe2ba8992c863c5103a0b2016dfa1a0
unknown <arixxdelera21@gmail.com>
2023-05-21 14:58:14 +0800

    exception handling error has been fixed

--- OOP_Calculator.py
+++ OOP_Calculator.py
@@ -36,8 +36,13 @@ class Calculator:
                     self.display.display_invalid_operation_message()
                     user_operation = self.input.get_operation()
 
-                number_1 = self.input.get_user_number("First")
-                number_2 = self.input.get_user_number("Second")
+                while True:
+                    try:
+                        number_1 = self.input.get_user_number("First")
+                        number_2 = self.input.get_user_number("Second")
+                        break
+                    except ValueError:
+                        print("\033[30mInvalid input. Please try again.\033[0m")
 
                 result = self.operations.calculate_result(user_operation, number_1, number_2)
 
@@ -46,18 +51,17 @@ class Calculator:
                 # display the result
                 self.display.display_result(result)
 
-                question = self.input.get_repeat_question()
-                if not self.input.is_repeat_question(question):
-                    break
-            
             # add the exception handling
-            except ValueError:
-                print("\033[30mInvalid input. Please try again.\033[0m")
             except ZeroDivisionError:
                 print("\033[30mInvalid Divisor. Zero is not applicable as a divisor!\033[0m")
+                continue
             except TypeError:
                 print("\033[30mInvalid data. Please try again.\033[0m")
-        
+                continue
+
+            question = self.input.get_repeat_question()
+            if not self.input.is_repeat_question(question):
+                break
         # display the outro
         self.display.display_goodbye_message()
 
