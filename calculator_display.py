8852819eabb01b878d3b1d07d386bde5751d1e21
unknown <arixxdelera21@gmail.com>
2023-05-21 13:01:50 +0800

    display psuedocode

---
+++ calculator_display.py
@@ -0,0 +1,12 @@
+# import pyfiglet, random, and time
+# create a class dispaly
+# use a Non-Parametrized Constructor
+# initialize the quotes
+# define the opening
+# define the display operations
+# define the random trivia\
+# define the processing message
+# define the display of the result
+# define the invalid operation
+# define the outro quote
+
