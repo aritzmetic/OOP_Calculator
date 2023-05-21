1bf99ca4de60a857cb5b30e6152de1419de62f91
unknown <arixxdelera21@gmail.com>
2023-05-21 13:00:30 +0800

    psuedocode pt.4

--- OOP_Calculator.py
+++ OOP_Calculator.py
@@ -1,6 +1,6 @@
 # Delera, Aritz B.
 
-# Import the calculator_input, calculator_operations
+# Import the calculator_input, calculator_operations, and calculator_display
 from calculator_input import CalculatorInput
 from calculator_operations import CalculatorOperations
 
@@ -8,14 +8,19 @@ from calculator_operations import CalculatorOperations
 class Calculator:
     # use a Non-Parametrized Constructor
     def __init__(self):
-        # initialize the calculator_input
+        # initialize the calculator_input, calculator_operations, and calculator_display
         self.input = CalculatorInput()
         self.operations = CalculatorOperations()
     
+    # display the introduction
+
     # test if numbers are able to output
     def test(self):
         # use while loop
         while True:
+            # display the operations
+            # display the random trivia
+
             user_operation = self.input.get_operation()
 
             # use try-except method for exception handling
@@ -25,15 +30,24 @@ class Calculator:
                     number_2 = self.input.get_user_number("Second")
 
                     result = self.operations.calculate_result(user_operation, number_1, number_2)
+
+                    # display the processing message
+                    # display the result
+
                     print(result)
 
                 else:
+                    # display invalid operations
                     print()
         
                 question = self.input.get_repeat_question()
                 if not self.input.is_repeat_question(question):
                     break
+            
+            # add the exception handling
             except ValueError:
                 print("Invalid operation. Please try again.")
+        
+        # display the outro
 calculator = Calculator()
 calculator.test()
