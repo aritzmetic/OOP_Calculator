fe7ad000e4d0da384dba8612e7edef425caacaaf
unknown <arixxdelera21@gmail.com>
2023-06-03 14:26:26 +0800

    pseudocode for new class is added

--- storing_calculator.py
+++ storing_calculator.py
@@ -1,1 +1,11 @@
+from OOP_Calculator import Calculator
+from calculator_operation import CalculatorOperations
+
 # Create a class for storing the result
+class StoringCalculator(Calculator, CalculatorOperations):
+    pass
+    # use a Non-Parametrized Constructor
+    # override the calculate_result method to store the history
+    # Add a new method to save the history to a file
+
+    
