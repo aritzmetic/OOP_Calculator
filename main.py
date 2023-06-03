ac274072713bbcca1e105e83daea6c774582294c
unknown <arixxdelera21@gmail.com>
2023-06-03 15:18:16 +0800

    storing_calculator has been filled

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from OOP_Calculator import Calculator
+from storing_calculator import StoringCalculator
 
 def run_calculator():
-    calculator = Calculator()
+    calculator = StoringCalculator()
     calculator.display_intro()
     calculator.test()
+    calculator.save_history_to_file()
 
 if __name__ == "__main__":
     run_calculator()
+    
--- storing_calculator.py
+++ storing_calculator.py
@@ -1,11 +1,23 @@
 from OOP_Calculator import Calculator
-from calculator_operation import CalculatorOperations
 
 # Create a class for storing the result
-class StoringCalculator(Calculator, CalculatorOperations):
-    pass
+class StoringCalculator(Calculator):
     # use a Non-Parametrized Constructor
-    # override the calculate_result method to store the history
-    # Add a new method to save the history to a file
+    def __init__(self):
+        super().__init__()
+        self.history = []
 
-    
+    # override the calculate_result method to store the history
+    def calculate_result(self, operator, number_1, number_2):
+        result = super().calculate_result(operator, number_1, number_2)
+        equation = f"{number_1} {operator} {number_2} = {result}"
+        self.history.append(equation)
+        return result
+
+    # Add a new method to save the history to a file
+    def save_history_to_file(self):
+        filename = "calculation_history.txt"
+        with open(filename, "w") as file:
+            for equation in self.history:
+                file.write(equation + "\n")
+        print(f"Calculation history saved to {filename}.")
